05222560749e49706727c8d7d67dedf3a9b6b1cb
filonsegundo <filonsegundo@hotmail.com>
2026-05-21 20:51:31 -0300

    submission: docker-compose, haproxy and info

--- src/Rinha.Api/Vector/VectorConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace Rinha.Api.Vector;
-
-public static class VectorConstants
-{
-    public const int Dims = 14;
-    public const int PackedDims = 16;
-    public const short Scale = 10000;
-}
--- src/Rinha.Preprocess/Reference.cs
+++
@@ -1,3 +0,0 @@
-namespace Rinha.Preprocess;
-
-public sealed record Reference(short[] Vector, byte Label);
